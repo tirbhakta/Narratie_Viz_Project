0038b3d44d49e89035b74733de1fbebdfbd6171a
TBhakta <70874876+tirbhakta@users.noreply.github.com>
2021-07-31 12:26:28 -0700

    Update scatterPlotWorld.js

--- d3 js files/scatterPlotWorld.js
+++ d3 js files/scatterPlotWorld.js
@@ -1,12 +1,10 @@
-
-
 function scatterPlotWorld(){
 
     // new graph
 	// Set the dimensions of the canvas / graph
-	var margin = {top: 2, right: 10, bottom: 40, left: 50},
-	    width = 520 - margin.left - margin.right,
-	    height = 520 - margin.top - margin.bottom;
+	var margin = {top: 20, right: 10, bottom: 40, left: 160},
+	    width = 820 - margin.left - margin.right,
+	    height = 420 - margin.top - margin.bottom;
 
 	// append the svg object to the body of the page
 	var svg = d3.select("#scatterplotDiv")
@@ -14,31 +12,29 @@ function scatterPlotWorld(){
     	.attr("width", width + margin.left + margin.right)
     	.attr("height", height + margin.top + margin.bottom)
   	.append("g")
-    	.attr("transform","translate(" + margin.left + "," + margin.top + ")");
-	
-	var color = d3.scaleOrdinal(d3.schemeCategory10);	
+    	.attr("transform","translate(" + margin.left + "," + margin.top + ")");	
 	
 	var div = d3.select("body").append("div")
      	.attr("class", "tooltip")
      	.style("opacity", 0);
 
 	// Read the data from csv
-	d3.csv("https://raw.githubusercontent.com/tirbhakta/Narrative_Viz_Project/e31a339a05e4eb3ea739d72847a70238aab8a0d1/World_Covid_Data.csv", function(error, data) {
-	  if (error) throw error;
-
+	d3.csv("https://raw.githubusercontent.com/tirbhakta/Narrative_Viz_Project/e31a339a05e4eb3ea739d72847a70238aab8a0d1/World_Covid_Data.csv", function(data) {
+	  
 		  // Add X axis
 	  	var x = d3.scaleLog()
-	    	.domain([0, 99999999])
+	    	.domain([10, 999999999])
 	    	.range([ 0, width ]);
 	  	svg.append("g")
 	    	.attr("transform", "translate(0," + height + ")")
 	    	.call(d3.axisBottom(x))
-		.select(".domain").remove();
+			.select(".domain").remove();
 
 	  	// Add Y axis
 	  	var y = d3.scaleLog()
-	    	.domain([0, 50000000])
-	    	.range([ height, 0]);
+	    	.domain([10, 999999999])
+	    	.range([ height, 0])
+			.nice();
 	  	svg.append("g")
 	    	.call(d3.axisLeft(y))
 		.select(".domain").remove();
@@ -56,7 +52,10 @@ function scatterPlotWorld(){
 	      	.attr("transform", "rotate(-90)")
 	      	.attr("y", -margin.left + 20)
 	      	.attr("x", -margin.top - height/2 + 20)
-	      	.text("Total Affected")
+	      	.text("Total Affected");
+			
+		// set color scale
+		var color = d3.scaleOrdinal(d3.schemeCategory40);
 		
 		// Add dots
   		svg.append('g')
@@ -67,7 +66,7 @@ function scatterPlotWorld(){
 	      	.attr("cx", function (d) { return x(d.total_recovered); } )
 	      	.attr("cy", function (d) { return y(d.total_confirmed); } )
 	      	.attr("r", 4.5)
-	      	.style("fill", function(d) { return color(d.location); })		
+	      	.style("fill", function(d,i) { return color(d.location); })		
 		// Add label on mouse-hover
 		.on('mouseover', function (d, i) {
 			d3.select(this).transition()
@@ -87,7 +86,7 @@ function scatterPlotWorld(){
           		div.transition()
                		.duration('200')
                		.style("opacity", 0);
-		});
+		})
 		
 	  });
 
