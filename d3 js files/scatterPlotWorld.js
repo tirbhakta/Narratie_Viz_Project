0949193d664037ca6884f253493e0b8ccfd8cba6
TBhakta <70874876+tirbhakta@users.noreply.github.com>
2021-07-31 14:21:23 -0700

    Update scatterPlotWorld.js

--- d3 js files/scatterPlotWorld.js
+++ d3 js files/scatterPlotWorld.js
@@ -112,7 +112,7 @@ function scatterPlotWorld(){
 		  	div.transition()
 		       .duration(100)
 		       .style("opacity", 1);
-		  	div.html(d)
+		  	div.html(d.location, d.total_recovered)
 		       .style("left", (d3.event.pageX + 10) + "px")
 		       .style("top", (d3.event.pageY - 15) + "px");
 	     	})  
